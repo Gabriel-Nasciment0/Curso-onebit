fdb9a42d572edf7ec760a667a6508c67d5ca48fe
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-12-11 10:43:00 -0300

    .

---
+++ fundamentos/javascript/aulas/aula04/index.js
@@ -0,0 +1,11 @@
+let velocidade = 80;
+
+while (velocidade > 0) {
+    alert(`O carro esta a ${velocidade} KM/h`);
+    velocidade -= 20;
+    alert("diminuindo velocidade");
+    if (velocidade === 40) {
+        break;
+    }
+}
+alert("parou");
