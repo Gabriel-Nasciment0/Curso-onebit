4e0808ab7217ff1de469be5d7d811e4bde634857
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2024-12-17 11:06:49 -0300

    .

--- fundamentos/javascript_II/exercicios/exercicio02/index.js
+++ fundamentos/javascript_II/exercicios/exercicio02/index.js
@@ -1,4 +1,57 @@
 let imoveis = [];
 let opcao = "";
+do {
+    opcao = prompt(`Bem vindo ao Cadastro de imóveis
+    Total de imóveis: ${imoveis.length}
+    Escolha uma opção:
+    1° Novo imóvel
+    2° Listar imóveis
+    3° Sair`);
 
-do {} while (opcao !== "3");
+    switch (opcao) {
+        case "1":
+            let imovel = {};
+            imovel.proprietario = prompt("Qual é o proprietário:");
+            imovel.quartos = parseFloat(prompt("Quantos quartos:"));
+            imovel.banheiros = parseFloat(prompt("Quantos banheiros:"));
+            imovel.garagem = prompt("Tem garagem? (sim/não)");
+
+            const confirma = confirm(`
+                Salvar imóvel?
+                Proprietário: ${imovel.proprietario}
+                Quartos: ${imovel.quartos}
+                Banheiros: ${imovel.banheiros}
+                Possui garagem? ${imovel.garagem}
+            `);
+
+            if (confirma) {
+                imoveis.push(imovel);
+            }
+            break;
+
+        case "2":
+            for (let i = 0; i < imoveis.length; i++) {
+                alert(
+                    "Imóvel " +
+                        (i + 1) +
+                        "\nProprietário: " +
+                        imoveis[i].proprietario +
+                        "\nQuartos: " +
+                        imoveis[i].quartos +
+                        "\nBanheiros: " +
+                        imoveis[i].banheiros +
+                        "\nPossui Garagem? " +
+                        imoveis[i].garagem
+                );
+            }
+            break;
+
+        case "3":
+            alert("Saindo...");
+            break;
+
+        default:
+            alert("Opção inválida, tente novamente.");
+            break;
+    }
+} while (opcao !== "3");
