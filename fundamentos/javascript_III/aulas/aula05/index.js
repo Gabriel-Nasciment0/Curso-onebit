05512738270861fb0704e373e071a3ca3a3047d2
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-01-10 11:39:39 -0300

    .

--- fundamentos/javascript_III/aulas/aula05/index.js
+++ fundamentos/javascript_III/aulas/aula05/index.js
@@ -1,5 +1,27 @@
+function register(ev) {
+    console.log(ev);
+    const sectionElement = ev.currentTarget.parentNode;
+
+    const username = sectionElement.children.username.value;
+    const password = sectionElement.children.password.value;
+    const passwordConfirmation =
+        sectionElement.children.passwordConfirmation.value;
+
+    if (password === passwordConfirmation) {
+        alert(`Usuario ${username} Registrado`);
+    } else {
+        alert(`As senhas sao diferentes`);
+    }
+}
+
 const button = document.getElementById("register-button");
 
-button.addEventListener("click", function () {
-    alert("Botão clicado");
+button.addEventListener("click", register);
+
+function removeEvent() {
+    button.removeEventListener("click", register);
+    alert("Evento removido");
+}
+button.addEventListener("mouseover", function (ev) {
+    console.log(ev.currentTarget);
 });
