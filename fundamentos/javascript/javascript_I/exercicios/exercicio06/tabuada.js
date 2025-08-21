7a3cac7a6a410bac9fa57f31675392a7702bf078
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-08-20 21:43:30 -0300

    .

--- fundamentos/javascript/javascript_I/exercicios/exercicio06/tabuada.js
+++ fundamentos/javascript/javascript_I/exercicios/exercicio06/tabuada.js
@@ -1,6 +1,6 @@
 let conta = parseFloat(prompt("escolha um numero a ser multplicado:"));
 let resultado = "";
-for (let index = 0; index < 21; index++) {
+for (let index = 0; index < 11; index++) {
     resultado += `${conta} x ${index} = ${conta * index} \n`;
 }
 alert(`Resultado da tabuada de ${conta}\n${resultado}`);
