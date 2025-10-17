2f857082ae567a8e5fde66d86c2d361fdf4aa75c
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-10-16 23:43:31 -0300

    .

--- fundamentos/javascript/javascript_VI/aulas/aula05/index.js
+++ fundamentos/javascript/javascript_VI/aulas/aula05/index.js
@@ -1,8 +1,40 @@
 // pendente - pending
 // resolvida - resolved
 // rejeitada - rejected
-const p = new Promise(() => {
-    console.log("a Promise esta sendo executada.");
-});
+// finalizada
+//exemplo explicativo
+// const p = new Promise((resolve, rejected) => {
+//     console.log("a Promise esta sendo executada.");
+//     setTimeout(() => {
+//         if (true) {
+//             rejected(false);
+//         }
+//         console.log("resolvendo a promise");
+//         resolve(true);
+//     }, 1000 * 2);
+// });
+
+// console.log(p);
+
+// setTimeout(() => {
+//     console.log(p);
+// }, 1000 * 3);
+// exemplo de usabilidade
+
+function execute() {
+    return new Promise((resolve, reject) => {
+        console.log("A promise está sendo executada.");
+        setTimeout(() => {
+            console.log("Resolvendo a promise...");
+            resolve("Resultado");
+        }, 2 * 1000);
+    });
+}
+
+const p = execute();
 
 console.log(p);
+
+setTimeout(() => {
+    console.log(p);
+}, 4 * 1000);
