fcfd168735728a67866ac9f7c86beb26a3de82ac
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-10-23 14:44:00 -0300

    .

--- fundamentos/javascript/javascript_VI/aulas/aula14/index.js
+++ fundamentos/javascript/javascript_VI/aulas/aula14/index.js
@@ -28,3 +28,27 @@ async function fetchArticle() {
 document.addEventListener("DOMContentLoaded", () => {
     fetchArticle();
 });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    const articleData = {
+        title: document.querySelector("#title").value,
+        author: document.querySelector("#author").value,
+        content: document.querySelector("#content").value,
+    };
+    const response = await fetch("http://localhost:3000/articles", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(articleData),
+    });
+
+    const savedArticle = await response.json();
+    form.reset();
+    renderArticle(savedArticle);
+    console.log(savedArticle);
+});
