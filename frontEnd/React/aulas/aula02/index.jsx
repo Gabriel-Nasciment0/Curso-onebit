d964c7f7a6b62517a26499fecd050819aaf5a126
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2025-12-30 09:15:17 -0300

    .

---
+++ frontEnd/React/aulas/aula02/index.jsx
@@ -0,0 +1,15 @@
+const element = <h1>Olà, mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <h1>texto</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <h2></h2>
+    </div>
+)
