21333cc5501c3dcd1beaa87168b5a038d0ca5961
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-01-02 18:30:04 -0300

    .

--- frontEnd/React/exercicios/ex03/src/App.jsx
+++ frontEnd/React/exercicios/ex03/src/App.jsx
@@ -3,13 +3,14 @@ import Profile from "./components/Profile/index"
 function App() {
     return (
         <>
-            <h1>ex03</h1>
             <Profile
                 name="John Doe"
                 bio="Full-stack javascript developer at Acme Inc."
                 phone="+5511987654321"
                 email="jonh.doe@email.com"
-                
+                githubUrl="GitHub"
+                linkedinUrl="Linkedin"
+                twitterUrl="Twitter"
             />
         </>
     )
