15215afc464e90a1ec73441a8deb23e91be950e3
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-01-03 19:00:13 -0300

    .

--- frontEnd/React/exercicios/ex03/src/App.jsx
+++ frontEnd/React/exercicios/ex03/src/App.jsx
@@ -1,9 +1,11 @@
 import Profile from "./components/Profile/index"
+import profileImg from "./assets/pexels-jit-roy-2028348030-31374567.jpg"
 
 function App() {
     return (
         <>
             <Profile
+                img={profileImg}
                 name="John Doe"
                 bio="Full-stack javascript developer at Acme Inc."
                 phone="+5511987654321"
