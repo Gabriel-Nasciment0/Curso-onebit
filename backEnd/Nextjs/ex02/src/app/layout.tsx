a9a859f91a15753d9344a3f8a7884884ce8f6bfc
Gabriel <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2026-06-13 12:45:51 -0300

    ex2 completo

---
+++ backEnd/Nextjs/ex02/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "./globals.scss"
+
+const inter = localFont({
+    src: "./fonts/Inter/Inter-VariableFont_opsz,wght.ttf",
+    variable: "--font-inter",
+    weight: "400 700",
+})
+
+const playfair = localFont({
+    src: "./fonts/Playfair_Display/PlayfairDisplay-VariableFont_wght.ttf",
+    variable: "--font-playfair",
+    weight: "100 400 700",
+})
+
+export const metadata: Metadata = {
+    title: "Next.js 13.4 - Exercício 02",
+    description: "Exercício 02 do curso de Next.js 13.4 da One Bit Code",
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.variable} ${playfair.variable}`}>
+                {children}
+            </body>
+        </html>
+    )
+}
